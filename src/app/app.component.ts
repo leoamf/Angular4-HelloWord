e037c41dabe7767db619c0b9abd9c0aab494da94
Leonardo Freitas <l.machado.de.freitas@avanade.com>
2018-05-18 14:18:31 -0300

    Iniciando alterações

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hello Word!!!!';
+  tasks = [ ];
+  task = '';
+  add() {
+    this.tasks.push(this.task);
+  }
+}
